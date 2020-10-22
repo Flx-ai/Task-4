583c753946e997e5b01f4b64d1b740cbbd83607c
Dmitry Kulinchenko <dima.kul22@mail.ru>
2020-10-23 00:08:52 +0300

    Task4

---
+++ src/vsu/cs/ru/num16/Main.java
@@ -0,0 +1,61 @@
+package vsu.cs.ru.num16;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int N = readValue('N');
+        int a = readValue('a');
+        printFinalSum(N, a);
+    }
+
+    private static int readValue(char name) {
+        System.out.printf("Input %s: ", name );
+        Scanner scan = new Scanner(System.in);
+        return scan.nextInt();
+    }
+
+    private static int calcInitialSum(int A1, int A2, int A3, int a) {
+        if (a > A3) {
+            return A1 + A2 + A3;
+        } else if ( a > A2) {
+            return A1 + A2;
+        } else if (a > A1) {
+            return A1;
+        }
+        else return 0;
+    }
+
+    private static int calcFinalSum(int N, int a) {
+        if (!checkInput(N)) {
+            System.out.println("Вы ввели некорректное N");
+            return 0;
+        }
+
+        int A1 = 1, A2 = 2, A3 = 3, Ak;
+        int sum = calcInitialSum(A1, A2, A3, a);
+
+        System.out.print("Список элементов: " + A1 + " " + A2 + " " + A3 + " ");
+
+        for (int k = 4; k <= N; k++) {
+            Ak = A3 + A2 - 2 * A1;
+            System.out.print(Ak + " ");
+            A1 = A2; A2 = A3; A3 = Ak;
+            if (Ak < a) {
+                sum += Ak;
+            }
+        }
+        return sum;
+    }
+
+    private static boolean checkInput(int N) {
+        return N > 2;
+    }
+
+    private static void printFinalSum(int N, int a) {
+        int sum = calcFinalSum(N, a);
+        System.out.print("\n" + "Сумма чисел = " + sum);
+    }
+}
+
+
